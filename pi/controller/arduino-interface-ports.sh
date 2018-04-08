68dcb53da8e185ef1f79388f7f7e3da3195a3253
Sebastian King <sebastian.king@btinternet.com>
2018-04-08 09:25:33 +0000

    sorted main & controller files

---
+++ pi/controller/arduino-interface-ports.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python arduino-interface-port.sh /dev/ttyACM0
+python arduino-interface-port.sh /dev/ttyACM1
+python arduino-interface-port.sh /dev/ttyACM2
+python arduino-interface-port.sh /dev/ttyACM3

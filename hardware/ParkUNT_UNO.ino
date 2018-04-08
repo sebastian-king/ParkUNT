8c82d54220c6d784880a4cfd5583a684e7bfe032
ntindle <nickandmason@hotmail.com>
2018-04-08 00:02:39 -0500

    Create ParkUNT_UNO.ino

---
+++ hardware/ParkUNT_UNO.ino
@@ -0,0 +1,31 @@
+//Number of parking spots
+const int numSpots=6;
+
+// These constants won't change:
+const int photo=A0;    // pin that the sensor is attached to
+
+const int ledPin = 9;        // pin that the LED is attached to
+
+// variables:
+int sensorValue=0;         // the sensor value
+
+void setup() {
+  
+  Serial.begin(19200); //Start Serial
+}
+
+void loop() {
+  
+    // read the sensor:
+    sensorValue = analogRead(photo);
+   
+    // fade the LED using the calibrated value:
+    analogWrite(ledPin, sensorValue);
+    
+    Serial.print(sensorValue);
+
+  Serial.println();
+  delay(200);
+  
+  
+}	

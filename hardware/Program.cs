87ef135c0d882bfeda44d0e06d84355cf7fdc64b
ntindle <nickandmason@hotmail.com>
2018-04-17 01:37:25 -0500

    Update Program.cs
    Add summary

--- hardware/Program.cs
+++ hardware/Program.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 using System.IO.Ports;
-using System.Net;
-using System.Text.RegularExpressions;
-using WebSocketSharp;
 
-namespace ParkUNTConsole
+namespace ParkUNT
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var comreader = new ArduinoSerialReader("COM5");
-
+            ListComPorts();
+            Console.WriteLine("Please enter the COM port to use: ");
+            var line = Console.ReadLine();
+            Console.WriteLine("Please enter the spot to be represented: ");
+            Int32.TryParse(Console.ReadLine(), out int spot);
+            var comreader = new ArduinoSerialReader(line, spot);
             Console.ReadLine();
+
         }
 
+        /// <summary>
+        /// Lists all com ports that are available to the computer
+        /// </summary>
         static void ListComPorts()
         {
             // Get a list of serial port names. 
@@ -33,76 +37,5 @@ namespace ParkUNTConsole
                 Console.WriteLine(port);
             }
         }
-
     }
-
-    public class ArduinoSerialReader : IDisposable
-    {
-        private SerialPort _serialPort;
-        private WebSocket ws;
-
-        private bool[] availability = { true, true, true, true, true, true, true };
-
-        /// <summary>
-        /// Evaluates the availability of a spot based upon sensor values
-        /// </summary>
-        /// <param name="value">A value to evaluate on</param>
-        public static bool EvaluateAvailability(int value) => value > 300 ? true : false;
-
-        public void EvaluateInputs(String str)
-        {
-            string[] stringArray = str.Split('-');
-            for (int i = 0; i < stringArray.Length; i++)
-            {
-                Int32.TryParse(stringArray[i], out int output);
-
-                if (EvaluateAvailability(output) != this.availability[i])
-                {
-                    this.availability[i] = !this.availability[i];
-                    StringBuilder stringBuilder = new StringBuilder("{ \"key\": \"update\", \"values\": { \"spot\": ").Append(i + 1).Append(", \"available\": ").Append(this.availability[i]?1:0).Append(" } }");
-
-                    if (this.ws.IsAlive) {
-                        this.ws.Send(stringBuilder.ToString());
-                    }
-                }
-
-            }
-
-            
-        }
-
-        public ArduinoSerialReader(string portName)
-        {
-            _serialPort = new SerialPort(portName)
-            {
-                BaudRate = 19200
-            };
-            _serialPort.Open();
-            _serialPort.DataReceived += SerialPort_DataReceived;
-
-            this.ws = new WebSocket("wss://websocket.parkunt.tech", "feed");
-            this.ws.Connect();
-        }
-
-        void SerialPort_DataReceived(object s, SerialDataReceivedEventArgs e)
-        {
-            
-            String str = _serialPort.ReadLine();
-
-            Console.WriteLine(str);
-
-            EvaluateInputs(str);
-        }
-
-        public void Dispose()
-        {
-            if (_serialPort != null)
-            {
-                _serialPort.Dispose();
-            }
-        }
-    }
-
-
 }
-
